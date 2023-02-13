0f888b94be715827b6206e4eb61f2d57bac10539
Berndzz <tukanbernad@gmail.com>
2023-02-13 23:43:15 +0700

    add new scipt for portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    container:{
+      center:true,
+      padding: '12px',
+    },
+    extend: {
+      colors:{
+        primary: '#7F52FF', //purple blue kotlin
+        dark: '#0f172a',
+        secondary: '#64748b',
+        third: '#cbd5e1'
+      },
+      screens:{
+        '2xl': '1320px',
+      }
+    },
   },
   plugins: [],
 }
